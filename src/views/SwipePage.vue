e5883be81fbcc535a1ec88566c54cbd983165722
RDDcat <yukmaro@gmail.com>
2023-07-27 19:19:07 +0900

    [UPDATE] 스와이프 컨텐츠 추가

--- src/views/SwipePage.vue
+++ src/views/SwipePage.vue
@@ -1,6 +1,22 @@
 <!-- 스와이프 페이지 -->
 <template>
-    <div>스와이프 page</div>
+    <div class="top">
+        <div class="top-title">스와이프</div>
+    </div>
+    <div class="content-imgtag">
+        <img class="content-imgtag" src="../assets/sample.png">
+    </div>
 </template>
 <script>
 </script>
+<style scoped>
+.content{
+    margin: auto; 
+}
+.content-imgtag{
+    margin: auto;   
+    width: 92vw;
+    height: 78vh;
+}
+
+</style>
