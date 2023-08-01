6f6b0de527b227a9f499dabb16cc7fbc63f74972
RDDcat <yukmaro@gmail.com>
2023-08-02 02:58:05 +0900

    [UPDATE] Adopt 페이지 일부 컴포넌트화

---
+++ src/components/AdoptBanner.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="top">
+        <img class='top-search-icon' src="../assets/sample.png" width="24">
+    </div>
+    <div class='banner'>
+        <div class='banner-content'>
+            <img class='banner-content-imgtag' src="../assets/sample.png">
+        </div>
+    </div>
+</template>
+<style scoped>
+.top-search-icon{
+    right: 4px;
+    position: absolute;
+}
+
+.banner{
+    width: 100vw;
+}
+
+.banner-content{
+    width: 100%;
+    height: 34vh;
+}
+
+.banner-content-imgtag{
+    width: 100%;
+    height: 100%;
+}
+</style>
--- src/views/AdoptPage.vue
+++ src/views/AdoptPage.vue
@@ -3,25 +3,9 @@
     <div class="top">
         <img class='top-search-icon' src="../assets/sample.png" width="24">
     </div>
-    <div class='banner'>
-        <div class='banner-content'>
-            <img class='banner-content-imgtag' src="../assets/sample.png">
-        </div>
-    </div>
-    <div class='category-twoline'>
-        <div class="category">
-            <div class="category-content" v-for="(category, index) in detailCategories_1" :key="index" @click="console.log(category)">
-                <img class="category-content-imgtag" src="../assets/sample.png">
-                <div class="category-content-text">{{category.text}}</div>
-            </div>
-        </div>
-        <div class="category">
-            <div class="category-content" v-for="(category, index) in detailCategories_2" :key="index" @click="console.log(category)">
-                <img class="category-content-imgtag" src="../assets/sample.png" >
-                <div class="category-content-text">{{category.text}}</div>
-            </div>
-        </div>
-    </div>
+    <AdoptBanner/>    
+    <CategoryTwoline />
+    
     <div class="content">
         <div class="content-card">
             <div class="content-card-img"><img class="content-card-imgtag" src="../assets/sample.png"></div>
@@ -37,35 +21,15 @@
 
 </template>
 <script>
+import CategoryTwoline from '../components/CategoryTwoline.vue'
+import AdoptBanner from '../components/AdoptBanner.vue'
+
+
 export default{
-    data(){
-        return{
-            detailCategories_1:[
-                {text: "파충류"},
-                {text: "양서류"},
-                {text: "풍뎅이"},
-                {text: "관상어"},
-                {text: "육지거북"},                
-                {text: "밀키프록"},
-                {text: "트리프록"},
-                {text: "사육용품"},
-                {text: "구독"},
-                {text: "영양제"},
-            ],
-            detailCategories_2:[
-                {text: "모니터"},
-                {text: "테구"},
-                {text: "호그노즈"},
-                {text: "장수풍뎅이"},
-                {text: "비어디"},
-                {text: "수생거북"},
-                {text: "밀키프록"},
-                {text: "팩맨"},
-                {text: "사육세트"},
-                {text: "먹이"},
-            ],
-        }
-    }
+    components: {
+        AdoptBanner,
+        CategoryTwoline,
+    },
 }
 </script>
 <style scoped>
@@ -74,19 +38,6 @@ export default{
     position: absolute;
 }
 
-.banner{
-    width: 100vw;
-}
-
-.banner-content{
-    width: 100%;
-    height: 34vh;
-}
-
-.banner-content-imgtag{
-    width: 100%;
-    height: 100%;
-}
 
 .category{
     display: flex;
