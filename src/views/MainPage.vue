014ae3b48dea6b4382dac6ae81777ef9e6fea51c
RDDcat <yukmaro@gmail.com>
2023-08-02 04:11:36 +0900

    [UPDATE] 메인페이지 컨텐츠 컴포넌트화

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -3,24 +3,15 @@
     <CategoryBasic />
     <BannerMain />
     <CategoryTwoline />
-    <div class='content-list'>
-        <div class="content" v-for="(content, index) in contents" :key="index">
-            <div class="content-title">
-                <div>{{content.title}}</div>
-                <div class="arrow-link" @click="console.log('이동')"><img src="../assets/sample.png" width="12"></div>
-            </div>
-            <div class="content-body">
-                <img src="../assets/sample.png" width="165" height="195">
-                <img src="../assets/sample.png" width="165" height="195">
-            </div>
-        </div>
-    </div>
+    <ContentTitleLinkTwoCard v-for="(content, index) in contents" :key="index" :content="content"/>
+    
 </template>
 <script>
 import TopMain from '../components/TopMain.vue'
 import CategoryBasic from '../components/CategoryBasic.vue'
 import BannerMain from '../components/BannerMain.vue'
 import CategoryTwoline from '../components/CategoryTwoline.vue'
+import ContentTitleLinkTwoCard from '../components/ContentTitleLinkTwoCard.vue'
 
 export default{
     components: {
@@ -28,34 +19,35 @@ export default{
         CategoryBasic,
         BannerMain,
         CategoryTwoline,
+        ContentTitleLinkTwoCard,
     },
     data(){
         return {
-            contents:[
+            contents: [],
+        }
+    },
+    methods: {
+        getContents(){
+            const dummyContents = [
                 {title:'최고의 귀염둥이!!'},
                 {title:'내 친구를 찾아보자'},
                 {title:'설마 여기까지 보겠어?'},
                 {title:'설마 여기까지 보겠어22?'},                
                 {title:'설마 여기까지 보겠어333?'},
             ]
+
+            return dummyContents;
         }
     },
-}
+    mounted() {
+        console.log(this.contents);
+        this.contents = this.getContents();
+        console.log(this.contents);
+    },
+};
 
 </script>
 <style scoped>
 
-
-
-.content-title{
-    display: flex;
-}
-.content-body{
-    display: flex;
-}
-
-.arrow-link{
-    margin-left: auto;
-}
 
 </style>
