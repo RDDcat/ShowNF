e99550e720c007c89f2eeee04929258f8e2e56ae
RDDcat <yukmaro@gmail.com>
2023-08-08 23:55:48 +0900

    [UPDATE] 마이페이지 내 게시글 컨텐츠 추가

--- src/components/MyPageMain.vue
+++ src/components/MyPageMain.vue
@@ -1,7 +1,41 @@
 <template>
-    <div>
-        asdfasdfasgwergaw
+    <div class="content">
+        <div class="item">
+            <img class="item-img" src="../assets/sample.png">
+        </div>
+        <div class="item">
+            <img class="item-img" src="../assets/sample.png">
+        </div>
+        <div class="item">
+            <img class="item-img" src="../assets/sample.png">
+        </div>
+        <div class="item">
+            <img class="item-img" src="../assets/sample.png">
+        </div>
+        <div class="item">
+            <img class="item-img" src="../assets/sample.png">
+        </div>
+        <div class="item">
+            <img class="item-img" src="../assets/sample.png">
+        </div>
     </div>
 </template>
 <script>
+
 </script>
+<style scoped>
+.content{
+    display: flex;
+    flex-wrap:wrap;
+    justify-content: center;
+}
+.item{
+    width: 33vw;
+    height: 33vw;
+}
+.item-img{
+    object-fit: contain;
+    width: 33vw;
+    height: 33vw;
+}
+</style>
