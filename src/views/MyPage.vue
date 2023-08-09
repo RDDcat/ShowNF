5bec554bb8530414f67ec5fe9886f975fc7aa695
RDDcat <yukmaro@gmail.com>
2023-08-09 14:38:52 +0900

    [UPDATE] 마이페이지 상단 프로필 컨텐츠 일부 추가

--- src/views/MyPage.vue
+++ src/views/MyPage.vue
@@ -2,10 +2,24 @@
 <template>
     <div class="top">
         <div class="top-title">마이페이지</div>
-        <div class="top-menu"><img src="../assets/sample.png"></div>
+        <img class="top-menu" src="../assets/sample.png">
     </div>
-    <div class="pofile">
-
+    <div class="pofile">        
+        <div>
+            <img class="pofile-img" src="../assets/sample.png">
+            <div>
+                네모네모
+            </div>
+        </div>
+        <div class="profile-info">
+            <div class="profile-info-user">
+                <div class="profile-info-username">백설공주</div>
+                <div class="profile-info-userid">@snow_white21</div>
+            </div>
+            <div class="profile-info-statemessage">
+                "거울아 거울아 나를 두고 가시나"
+            </div>
+        </div>
     </div>
     <div class="xp">
 
@@ -36,8 +50,25 @@ export default{
 };
 </script>
 <style scoped>
-.top-menu{
-    width: 6vw;
+.top{
+    display: flex;
 }
-
+.top-title{
+    font-size: 18px;
+    margin: auto;
+}
+.top-menu{
+    width: 24px;
+    margin: 4px;
+}
+.pofile-img{    
+    margin: 16px;    
+    width: 72px;
+}
+.profile-info{
+    display: flex;
+}
+.profile-info-statemessage{
+    margin: auto;
+}
 </style>
