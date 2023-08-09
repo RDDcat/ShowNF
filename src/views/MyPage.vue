1f73ef44459e337439ba9c2eede6a5198c0467f9
RDDcat <yukmaro@gmail.com>
2023-08-09 14:55:11 +0900

    [UPDATE] 마이페이지 상단 프로필 컨텐츠 추가

--- src/views/MyPage.vue
+++ src/views/MyPage.vue
@@ -4,11 +4,31 @@
         <div class="top-title">마이페이지</div>
         <img class="top-menu" src="../assets/sample.png">
     </div>
-    <div class="pofile">        
-        <div>
+    <div class="pofile">
+        <div class="pofile-static">
             <img class="pofile-img" src="../assets/sample.png">
-            <div>
-                네모네모
+            <div class="pofile-statics">
+                <div class="pofile-statics-card">
+                    <div class="pofile-statics-card-title">활동일수</div>
+                    <div class="pofile-statics-card-content">+182일</div>
+                </div>
+                <div class="pofile-statics-card">
+                    <div class="pofile-statics-card-title">팔로워</div>
+                    <div class="pofile-statics-card-content">823</div>
+                </div>
+                <div class="pofile-statics-card">
+                    <div class="pofile-statics-card-title">팔로잉</div>
+                    <div class="pofile-statics-card-content">1198</div>
+                </div>
+                <div class="pofile-statics-card">
+                    <div class="pofile-statics-card-title">게시물</div>
+                    <div class="pofile-statics-card-content">219</div>
+                </div>
+                <div class="pofile-statics-card">
+                    <div class="pofile-statics-card-title">거래완료</div>
+                    <div class="pofile-statics-card-content">38</div>
+                </div>
+                
             </div>
         </div>
         <div class="profile-info">
@@ -61,9 +81,36 @@ export default{
     width: 24px;
     margin: 4px;
 }
+.pofile-static{
+    display: flex;
+}
 .pofile-img{    
     margin: 16px;    
     width: 72px;
+}
+.pofile-statics{
+    display: flex;
+}
+.pofile-statics-card{
+    border-radius: 5px;
+    background-color: #f5f5f5;
+    border: 1px soild #d5d5d5;
+    width: 64px;
+    height: 52px;
+    margin: auto;    
+    margin-right: 2px;
+    margin-left: 1px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+.pofile-statics-card-title{
+    font-size: 10px;
+    margin-bottom: 2px;
+}
+.pofile-statics-card-content{
+    font-size: 14px;
 }
 .profile-info{
     display: flex;
