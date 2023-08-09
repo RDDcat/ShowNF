138937608e662f7200856123373d4c9139dc73e9
RDDcat <yukmaro@gmail.com>
2023-08-09 11:11:34 +0900

    [UPDATE] 마이페이지 내 앨범 탭 필터 추가

--- src/components/MyPageAlbum.vue
+++ src/components/MyPageAlbum.vue
@@ -1,7 +1,34 @@
 <template>
-    <div>
-        asdfasdfasgwergaw
+    <div class="filters">
+        <div class="filter">
+            <img class="filter-img" src="../assets/sample.png">
+            <div class="filter-text">게시글</div>
+        </div>
+        <div class="filter">
+            <img class="filter-img" src="../assets/sample.png">
+            <div class="filter-text">사진</div>
+        </div>
     </div>
 </template>
 <script>
 </script>
+<style scoped>
+.filters{
+    display: flex;
+    justify-content: flex-end;
+    margin-top: 4px;
+    margin-bottom: 4px;
+}
+.filter{
+    display: flex;
+    justify-content: flex-end;
+}
+.filter-img{
+    width: 12px;
+    margin-right: 2px;
+}
+.filter-text{
+    font-size: 8px;
+    margin-right: 6px;
+}
+</style>
