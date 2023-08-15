469a1a3b80f008aadc1111f583db4375645a87ad
RDDcat <yukmaro@gmail.com>
2023-08-15 15:02:23 +0900

    [UPDATE] 랭킹페이지 컨텐츠 추가

--- src/views/views/LeaderPage.vue
+++ src/views/views/LeaderPage.vue
@@ -4,10 +4,16 @@
         <div class="top-title">랭킹</div>        
         <div class="balance"></div>
     </div>
+    <div class="nav-ranks">
+        <div class="nav-rank">
+
+        </div>
+    </div>
     <div class="cards">
-        <div class="card-title">이용내역</div>
         <div class="card">
-            
+            <img class="card-img" src="assets/logo.png" width='50' height="50">
+            <div class="card-username">NickName</div>
+            <div class="card-rating">rating: 21223</div>
         </div>
     </div>
 </template>
@@ -44,6 +50,33 @@ export default{
 }
 .balance{
     width: 32px;
+}
+
+.card{
+    margin:15px;
+    border-radius: 10px;
+    width: 90vw;
+    height: 75px;
+    background-color: #d9d9d9;
+    display: flex;
+}
+.card-img{
+    margin-top: auto;
+    margin-bottom: auto;
+    margin-left: 12px;
+    margin-right: 12px;
+}
+.card-username{
+    margin-top: auto;
+    margin-bottom: auto;
+    margin-right: auto;
+    font-size: 16px;
+}
+.card-rating{
+    margin-top: auto;
+    margin-bottom: auto;
+    margin-right: 16px;
+    font-size: 14px;
 }
 
 
