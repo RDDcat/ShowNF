bc61f9e244617e745874b5397d6ea0410dc4b654
RDDcat <yukmaro@gmail.com>
2023-08-22 19:27:12 +0900

    [UPDATE] 마이펫 캘린더 컨탠츠 추가 및 링크 연결

--- src/views/MyPet/MyPetCalender.vue
+++ src/views/MyPet/MyPetCalender.vue
@@ -1,10 +1,41 @@
 <template>
-    <div class="contents">
-
+    <div class="calender">
+        <div class="calender-frame">
+            <div class="calender-top">
+                <div class="calender-top-left-arrow"><img src="assets/logo.png" width="16" height="16"></div>
+                <div class="calender-top-date">2023년 9월</div>
+                <div class="calender-top-right-arrow"><img src="assets/logo.png" width="16" height="16"></div>
+            </div>
+        </div>
     </div>
 </template>
 <script>
 </script>
 <style scoped>
-
+.calender{
+    display: flex;
+    width: 100vw;
+    height: 46vh;
+}
+.calender-frame{
+    margin: auto;
+    width: 97%;
+    height: 97%;
+    border: 1px solid #ACACB1;
+    border-radius: 10px;
+}
+.calender-top{
+    display: flex;
+}
+.calender-top-left-arrow{
+    margin-left: 16px;
+    margin-top: 10px;
+}
+.calender-top-date{
+    margin: auto;
+}
+.calender-top-right-arrow{
+    margin-right: 16px;
+    margin-top: 10px;
+}
 </style>
--- src/views/MyPet/MyPetMain.vue
+++ src/views/MyPet/MyPetMain.vue
@@ -1,35 +1,35 @@
 <template>
     <div class="pet-cards">
         <div class="pet-card">
-            <div class="pet-card-frame" @click="console.log('ASdf1')">
+            <div class="pet-card-frame" @click="movePage('/pet/calender')">
                 <div class="pet-card-img">
                     <img src="assets/logo.png" width="56" height="56">
                 </div>
             </div>
         </div>
         <div class="pet-card">
-            <div class="pet-card-frame" @click="console.log('ASdf2')">
+            <div class="pet-card-frame" @click="movePage('/pet/calender')">
                 <div class="pet-card-img">
                     <img src="assets/logo.png" width="56" height="56">
                 </div>
             </div>
         </div>
         <div class="pet-card">
-            <div class="pet-card-frame" @click="console.log('ASdf3')">
+            <div class="pet-card-frame" @click="movePage('/pet/calender')">
                 <div class="pet-card-img">
                     <img src="assets/logo.png" width="56" height="56">
                 </div>
             </div>
         </div>
         <div class="pet-card">
-            <div class="pet-card-frame" @click="console.log('ASdf4')">
+            <div class="pet-card-frame" @click="movePage('/pet/calender')">
                 <div class="pet-card-img">
                     <img src="assets/logo.png" width="56" height="56">
                 </div>
             </div>
         </div>
         <div class="pet-card">
-            <div class="pet-card-frame" @click="console.log('ASdf5')">
+            <div class="pet-card-frame" @click="movePage('/pet/calender')">
                 <div class="pet-card-img">
                     <img src="assets/logo.png" width="56" height="56">
                 </div>
@@ -55,61 +55,6 @@ export default {
 
 </script>
 <style scoped>
-.top{
-    display: flex;
-}
-.top-title{
-    margin: auto;
-    margin-top: 1vh;
-    justify-content: center;
-    font-size: 20px;
-    height: 40px;
-}
-
-.frame{
-    display: flex;
-    height: 36vh;
-    border: 1px solid rgb(166, 168, 175);
-
-}
-.main-frame{
-    display: flex;
-    flex-direction: column;
-    margin:auto;
-}
-.main-frame-img{
-    z-index: 99;
-}
-.main-frame-img-shadow{
-    position: relative;
-    top: auto;
-    left: auto;
-    bottom: 10px;
-    right: auto;
-    text-align: center;
-}
-.main-frame-pet-name{
-    position: relative;
-    top: 4vh;
-    left: auto;
-    bottom: auto;
-    right: auto;
-    text-align: center;
-}
-
-
-
-.left-arrow{
-    margin-left: 1.5vw;
-    margin-top: auto;
-    margin-bottom: auto;
-}
-.right-arrow{
-    margin-right: 1.5vw;
-    margin-top: auto;
-    margin-bottom: auto;
-}
-
 .pet-cards{
     display: flex;
     flex-wrap : wrap;
