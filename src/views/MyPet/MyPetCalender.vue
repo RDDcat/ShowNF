d1b148a3218f1539362e511fe986bf2e60fd4ba8
RDDcat <yukmaro@gmail.com>
2023-08-22 23:08:15 +0900

    [UPDATE] 캘린더 컨텐츠 추가 (미완)

--- src/views/MyPet/MyPetCalender.vue
+++ src/views/MyPet/MyPetCalender.vue
@@ -7,116 +7,132 @@
                 <div class="calender-top-right-arrow"><img src="assets/logo.png" width="16" height="16"></div>
             </div>
             <div class="calender-contents">
-                <div class="calender-content">
+                <div class="calender-content" @click="movePage('diary/add')">
+                    <div class="calender-content-text"></div>
+                </div>
+                <div class="calender-content" @click="movePage('diary/add')">
+                    <div class="calender-content-text"></div>
+                </div>
+                <div class="calender-content" @click="movePage('diary/add')">
+                    <div class="calender-content-text"></div>
+                </div>
+                <div class="calender-content" @click="movePage('diary/add')">
+                    <div class="calender-content-text"></div>
+                </div>
+                <div class="calender-content" @click="movePage('diary/add')">
+                    <div class="calender-content-text"></div>
+                </div>
+                <div class="calender-content" @click="movePage('diary/add')">
                     <div class="calender-content-text">1</div>
                 </div>
-                <div class="calender-content">
-                    <div class="calender-content-text">1</div>
+                <div class="calender-content" @click="movePage('diary/add')">
+                    <div class="calender-content-text">2</div>
                 </div>
-                <div class="calender-content">
-                    <div class="calender-content-text">1</div>
+                
+                <div class="calender-content" @click="movePage('diary/add')">
+                    <div class="calender-content-text">3</div>
                 </div>
-                <div class="calender-content">
-                    <div class="calender-content-text">1</div>
+                <div class="calender-content" @click="movePage('diary/add')">
+                    <div class="calender-content-text">4</div>
                 </div>
-                <div class="calender-content">
-                    <div class="calender-content-text">1</div>
+                <div class="calender-content" @click="movePage('diary/add')">
+                    <div class="calender-content-text">5</div>
                 </div>
-                <div class="calender-content">
-                    <div class="calender-content-text">1</div>
+                <div class="calender-content" @click="movePage('diary/add')">
+                    <div class="calender-content-text">6</div>
                 </div>
-                <div class="calender-content">
-                    <div class="calender-content-text">1</div>
+                <div class="calender-content" @click="movePage('diary/add')">
+                    <div class="calender-content-text">7</div>
                 </div>
-                <div class="calender-content">
-                    <div class="calender-content-text">1</div>
+                <div class="calender-content" @click="movePage('diary/add')">
+                    <div class="calender-content-text">8</div>
                 </div>
-                <div class="calender-content">
-                    <div class="calender-content-text">1</div>
+                <div class="calender-content" @click="movePage('diary/add')">
+                    <div class="calender-content-text">9</div>
                 </div>
-                <div class="calender-content">
-                    <div class="calender-content-text">1</div>
+
+                <div class="calender-content" @click="movePage('diary/add')">
+                    <div class="calender-content-text">10</div>
                 </div>
-                <div class="calender-content">
-                    <div class="calender-content-text">1</div>
-                </div>                
-                <div class="calender-content">
-                    <div class="calender-content-text">1</div>
-                </div>                
-                <div class="calender-content">
-                    <div class="calender-content-text">1</div>
-                </div>                
-                <div class="calender-content">
-                    <div class="calender-content-text">1</div>
-                </div>                
-                <div class="calender-content">
-                    <div class="calender-content-text">1</div>
-                </div>                
-                <div class="calender-content">
-                    <div class="calender-content-text">1</div>
-                </div>                
-                <div class="calender-content">
-                    <div class="calender-content-text">1</div>
-                </div>                
-                <div class="calender-content">
-                    <div class="calender-content-text">1</div>
-                </div>                
-                <div class="calender-content">
-                    <div class="calender-content-text">1</div>
-                </div>                
-                <div class="calender-content">
-                    <div class="calender-content-text">1</div>
-                </div>                
-                <div class="calender-content">
-                    <div class="calender-content-text">1</div>
+                <div class="calender-content" @click="movePage('diary/add')">
+                    <div class="calender-content-text">11</div>
                 </div>
-<div class="calender-content">
-                    <div class="calender-content-text">1</div>
+                <div class="calender-content" @click="movePage('diary/add')">
+                    <div class="calender-content-text">12</div>
                 </div>
-                <div class="calender-content">
-                    <div class="calender-content-text">1</div>
+                <div class="calender-content" @click="movePage('diary/add')">
+                    <div class="calender-content-text">13</div>
                 </div>
-                <div class="calender-content">
-                    <div class="calender-content-text">1</div>
+                <div class="calender-content" @click="movePage('diary/add')">
+                    <div class="calender-content-text">14</div>
                 </div>
-                <div class="calender-content">
-                    <div class="calender-content-text">1</div>
+                <div class="calender-content" @click="movePage('diary/add')">
+                    <div class="calender-content-text">15</div>
                 </div>
-                <div class="calender-content">
-                    <div class="calender-content-text">1</div>
+                <div class="calender-content" @click="movePage('diary/add')">
+                    <div class="calender-content-text">16</div>
                 </div>
-                <div class="calender-content">
-                    <div class="calender-content-text">1</div>
+
+                <div class="calender-content" @click="movePage('diary/add')">
+                    <div class="calender-content-text">10</div>
                 </div>
-                <div class="calender-content">
-                    <div class="calender-content-text">1</div>
+                <div class="calender-content" @click="movePage('diary/add')">
+                    <div class="calender-content-text">11</div>
                 </div>
-                <div class="calender-content">
-                    <div class="calender-content-text">1</div>
+                <div class="calender-content" @click="movePage('diary/add')">
+                    <div class="calender-content-text">12</div>
                 </div>
-                <div class="calender-content">
-                    <div class="calender-content-text">1</div>
+                <div class="calender-content" @click="movePage('diary/add')">
+                    <div class="calender-content-text">13</div>
                 </div>
-                <div class="calender-content">
-                    <div class="calender-content-text">1</div>
+                <div class="calender-content" @click="movePage('diary/add')">
+                    <div class="calender-content-text">14</div>
                 </div>
-                <div class="calender-content">
-                    <div class="calender-content-text">1</div>
-                </div>                
-                <div class="calender-content">
-                    <div class="calender-content-text">1</div>
-                </div>                
-                <div class="calender-content">
-                    <div class="calender-content-text">1</div>
-                </div>                
-                <div class="calender-content">
-                    <div class="calender-content-text">1</div>
+                <div class="calender-content" @click="movePage('diary/add')">
+                    <div class="calender-content-text">15</div>
+                </div>
+                <div class="calender-content" @click="movePage('diary/add')">
+                    <div class="calender-content-text">16</div>
+                </div>
+
+                <div class="calender-content" @click="movePage('diary/add')">
+                    <div class="calender-content-text">10</div>
+                </div>
+                <div class="calender-content" @click="movePage('diary/add')">
+                    <div class="calender-content-text">11</div>
+                </div>
+                <div class="calender-content" @click="movePage('diary/add')">
+                    <div class="calender-content-text">12</div>
+                </div>
+                <div class="calender-content" @click="movePage('diary/add')">
+                    <div class="calender-content-text">13</div>
+                </div>
+                <div class="calender-content" @click="movePage('diary/add')">
+                    <div class="calender-content-text">14</div>
+                </div>
+                <div class="calender-content" @click="movePage('diary/add')">
+                    <div class="calender-content-text">15</div>
+                </div>
+                <div class="calender-content" @click="movePage('diary/add')">
+                    <div class="calender-content-text">16</div>
                 </div>
             </div>
         </div>
     </div>
 </template>
 <script>
+export default {
+    data(){
+        return {
+            
+        }
+    },
+    methods: {
+        movePage(url){
+            this.$router.push(url);
+        },
+    },
+}
 </script>
 <style scoped>
 .calender{
@@ -133,26 +149,40 @@
 }
 .calender-top{
     display: flex;
+    width: 100%;
+    height: 10%;
 }
 .calender-top-left-arrow{
     margin-left: 16px;
-    margin-top: 10px;
+    margin-top: auto;
+    margin-bottom: auto;
 }
 .calender-top-date{
     margin: auto;
 }
 .calender-top-right-arrow{
     margin-right: 16px;
-    margin-top: 10px;
+    margin-top: auto;
+    margin-bottom: auto;
 }
 
 .calender-contents{
     display: flex;
     flex-wrap : wrap;
+    width: 100%;
+    height: 90%;
+    margin-top: auto;
+    margin-bottom: auto;
+    
 }
 .calender-content{
-    width: 14.2%;
+    margin: auto;
+    width: 13vw;
+    height: 13vw;
     display: flex;
+    border-radius: 8px;
+    background-color: #f6f6f9;
+
 }
 .calender-content-text{
     margin: auto;
