6e257c400a17bdc5ef9c4103c57f25bcd9b612fe
RDDcat <yukmaro@gmail.com>
2023-09-10 22:43:51 +0900

    [UPDATE] 스와이프 페이지 이미지 받아와서 클릭 이벤트에 하나씩 지워지게 작성
    기본 틀 작성완료

--- src/views/SwipePage.vue
+++ src/views/SwipePage.vue
@@ -1,12 +1,16 @@
 <!-- 스와이프 페이지 -->
 <template>
     <div class="top">
-        <div class="top-title" @click="pringData">스와이프</div>
+        <div class="top-title">스와이프</div>
         <div class="mybox"><img src="assets/logo.png" @click="movePage('/storage/album')"></div>
     </div>
     <div class="content-imgtag">
-        <img :src="content[0].imageUrl">
-        {{this.content}}
+        <img 
+            v-for="(content, index) in contents" :key="index"
+            v-show="contents_show[index]"
+            :src="content.imageUrl"
+            @click='swipeImage(index)'
+            >
     </div>
 </template>
 <script>
@@ -16,12 +20,19 @@ export default{
     data(){
         return {
             data: [],
-            content: [],
+            contents: [],
+            contents_show: [],
         }
     },
     methods: {
-        pringData(){
-            console.log("data : ", this.data);
+        swipeImage(index){
+            // 스와이프 동작
+            // 현재 이미지 가리기
+            // 이미지 이벤트 전송
+            this.contents_show[index] = false;
+
+            // 페이저블, 데이터 더 가져오기
+
 
         },
         movePage(url){
@@ -33,7 +44,8 @@ export default{
         .then(response=>{
             console.log("getSwipeImage : ", response);
             this.data = response.data;
-            this.content = Array.from(this.data.content);
+            this.contents = Array.from(this.data.content);            
+            this.contents_show = new Array(this.contents.length).fill(true);
         })
         .catch(function (e){
             console.log(e);
@@ -66,6 +78,11 @@ export default{
 }
 .content-imgtag{
     margin: auto;   
+    width: 92vw;
+    height: 78vh;
+}
+.content-imgtag img{
+    position: fixed;
     width: 92vw;
     height: 78vh;
 }
