865247bd1bcf0d4609e15ee206bcd35693e5850f
RDDcat <yukmaro@gmail.com>
2023-09-14 15:23:51 +0900

    [UPDATE] 분양게시글 조회 페이지 css 수정

--- src/views/Adopt/AdoptPostPage.vue
+++ src/views/Adopt/AdoptPostPage.vue
@@ -15,13 +15,38 @@
     </div>
 
     <!-- 조회 게시글 내용 -->
-    
-    <p>제목(개체명) {{data.individual}}</p>
-    <p>생일: {{data.birthday}}</p>
-    <p>종: {{data.firstSpecies}}</p>
-    <p>종: {{data.secondSpecies}}</p>
-    <p>무게 : {{data.weight}}</p>
-    <p>가격 : {{data.price}}</p>
+    <div class="individual">
+        <div class="individual-title">제목</div>
+        <div class="individual-text">{{data.individual}}</div>
+    </div>
+    <div class="species">
+        <div class="species-first">
+            <div class="species-first-title">분류1</div>
+            <div class="species-first-text">{{data.firstSpecies}}</div>
+        </div>
+        <div class="species-second">
+            <div class="species-second-title">분류2</div>
+            <div class="species-second-text">{{data.secondSpecies}}</div>
+        </div>
+    </div>
+    <div class="birthday">
+        <div class="birthday-title">생일</div>
+        <div class="birthday-text">{{data.birthday}}</div>
+    </div>
+    <div class="weight">
+        <div class="weight-title">생일</div>
+        <div class="weight-text">{{data.weight}}</div>
+    </div>
+    <div class="memo">
+        <div class="memo-title">설명</div>
+        <div class="memo-text">{{data.speciesExplain}}</div>
+    </div>
+    <div class="price">
+        <div class="price-title">가격</div>
+        <div class="price-text">{{data.price}}</div>
+    </div>
+
+
     <p>좋아요 : {{data.heartCount}}</p>
 
     <!-- 하단 플로팅 바 (좋아요, 채팅하기) -->
@@ -101,6 +126,116 @@ export default {
 .image img{
     width: 100vw;
     height: 300px;
-    object-fit: contain;
+    object-fit: cover;
+}
+
+.individual{
+    margin-left: 12px;    
+    margin-bottom: 12px;
+    
+}
+.individual-title{
+    margin-top: 8px;
+    font-size: 12px;
+}
+.individual-text{
+    margin-top: 8px;
+    margin-bottom: 8px;
+    font-size: 18px;
+}
+.species{    
+    display: flex;
+    margin-top: 8px;   
+    margin-bottom: 8px;   
+}
+.species-first{
+    width: 50vw;
+}
+.species-first-title{
+    margin: 4px;
+    margin-left: 12px;    
+    font-size: 12px;
+}
+.species-first-text{
+    margin: 4px;
+    margin-top: 8px;
+    margin-left: 12px;
+    font-size: 18px;
+
+}
+.species-second{
+    width: 46vw;
+}
+.species-second-title{
+    margin: 4px;
+    margin-left: 12px;    
+    font-size: 12px;
+
+}
+.species-second-text{
+    margin: 4px;
+    margin-top: 8px;
+    margin-left: 12px;
+    font-size: 18px;
+}
+
+.birthday{
+    margin-left: 12px;    
+    margin-bottom: 12px;
+    
+}
+.birthday-title{
+    margin-top: 8px;
+    font-size: 12px;
+}
+.birthday-text{
+    margin-top: 8px;
+    margin-bottom: 8px;
+    font-size: 18px;
+}
+
+.weight{
+    margin-left: 12px;    
+    margin-bottom: 12px;
+    
+}
+.weight-title{
+    margin-top: 8px;
+    font-size: 12px;
+}
+.weight-text{
+    margin-top: 8px;
+    margin-bottom: 8px;
+    font-size: 18px;
+}
+
+.memo{
+    margin-left: 12px;    
+    margin-bottom: 12px;
+    
+}
+.memo-title{
+    margin-top: 8px;
+    font-size: 12px;
+}
+.memo-text{
+    margin-top: 8px;
+    margin-bottom: 8px;
+    font-size: 16px;
+}
+
+.price{
+    margin-left: 12px;    
+    margin-bottom: 12px;
+    
+}
+.price-title{
+    margin-top: 8px;
+    font-size: 12px;
+}
+.price-text{
+    margin-top: 8px;
+    margin-bottom: 8px;
+    font-size: 18px;
 }
 </style>
